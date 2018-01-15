17581dcbb41457381300a6f7fa366f4f60f92619
stanleyunderwood <33239531+stanleyunderwood@users.noreply.github.com>
2018-01-15 17:57:48 -0600

    add key

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:blogz@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
-
+app.secret_key = '123abc!'
 
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key = True)
